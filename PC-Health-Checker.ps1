e395a4c9e2d534b2bed328be570cb82345ce694e
naseemkhatol <meesan321@gmail.com>
2026-08-11 19:12:16 -0400

    Update PC-Health-Checker.ps1
    Add disk health report

--- PC-Health-Checker.ps1
+++ PC-Health-Checker.ps1
@@ -1,1 +1,21 @@
+# ==========================================
+# PowerShell PC Health Checker
+# ==========================================
 
+Write-Host "=========================================="
+Write-Host "        PC HEALTH CHECK REPORT"
+Write-Host "=========================================="
+
+# ==========================================
+# DISK REPORT
+# ==========================================
+
+$Drive = Get-Volume | Where-Object DriveLetter -eq "C"
+
+Write-Host ""
+Write-Host "==== DISK REPORT ===="
+
+Write-Host "Drive: $($Drive.DriveLetter)"
+Write-Host "Total Size: $("{0:N0}" -f ($Drive.Size / 1GB)) GB"
+Write-Host "Free Space: $("{0:N0}" -f ($Drive.SizeRemaining / 1GB)) GB"
+Write-Host "Free Space %: $("{0:P0}" -f ($Drive.SizeRemaining / $Drive.Size))"
